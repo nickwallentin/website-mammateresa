fdd6697b594d4aca15018e2cc01357a26f24c6a5
Niklas Wallentin <niklaswallentin@Niklass-MacBook-Pro.local>
2019-09-26 15:14:27 +0200

    fixed building fail

--- src/pages/meny/avec.js
+++ src/pages/meny/avec.js
@@ -3,7 +3,7 @@ import React from "react"
 import Layout from "../../components/layout"
 import { Sec, Wrap } from "../../components/styled"
 
-import AvecMenu from "../../components/menu/Avecmenu"
+import AvecMenu from "../../components/menu/avecmenu"
 
 const AvecPage = () => {
   return (
